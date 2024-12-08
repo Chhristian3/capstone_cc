224fa9e499525970fdfe41a688b303b40e3d013f
Chunchunmaru26 <adriandejesus122601@gmail.com>
2024-12-08 20:17:52 +0800

    added roles and admin page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,17 @@
+import { NextResponse } from "next/server"
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
 
-const isProtectedRoute = createRouteMatcher(["/client(.*)"])
+const isAdminRoute = createRouteMatcher(["/admin(.*)"])
 
 export default clerkMiddleware(async (auth, req) => {
-  if (isProtectedRoute(req)) await auth.protect()
+  // Protect all routes starting with `/admin`
+  if (
+    isAdminRoute(req) &&
+    (await auth()).sessionClaims?.metadata?.role !== "admin"
+  ) {
+    const url = new URL("/", req.url)
+    return NextResponse.redirect(url)
+  }
 })
 
 export const config = {
